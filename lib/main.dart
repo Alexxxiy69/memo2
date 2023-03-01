94be5298507e46527d77acdeec900b0c798d8860
Alexxxiy69 <0705203@gmail.com>
2023-03-01 18:50:46 +0300

    made so two identical cards turn grey (now i need for them to never close)

--- lib/main.dart
+++ lib/main.dart
@@ -24,30 +24,60 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> {
   List<bool> show = [false, false, false, false];
   List<int> rand = [0, 0, 0, 0];
+  int card2 = 0;
+  var cardColor = [Colors.blue, Colors.blue, Colors.blue, Colors.blue];
+  int opened = 0;
+  int iOld = 0;
 
   void pressing(i) {
+    int card1;
+    card1 = rand[i];
+    if (opened == 2) {
+      setState(() {
+        show = [false, false, false, false];
+      });
+      opened = 0;
+    }
     setState(() {
       show[i] = !show[i];
     });
+    if(opened == 1) {
+      setState(() {
+        if(card1 == card2) {
+          cardColor[i] = Colors.grey;
+          cardColor[iOld] = Colors.grey;
+          final show[i] = true;
+          final show[iOld] = true;
+        }
+      });
+    }
+    opened++;
+    card2 = card1;
+    iOld = i;
   }
 
   void rando() {
-    for(int i = 0; i < 3; i++) {
-      int ik = Random().nextInt(1) + 2;
-      if(rand.howMuch(ik)  < 2) {
+    int returned;
+    for (int i = 0; i < 4; i++) {
+      int ik = Random().nextInt(2) + 1;
+      returned = howMuch(rand, ik);
+      if (returned < 2) {
         rand[i] = ik;
+      } else{
+        i--;
       }
     }
   }
 
   Widget cards(i) {
-    rando();
-
+    if (rand[0] == 0) {
+      rando();
+    }
     return Padding(
       padding: EdgeInsets.all(10),
       child: TextButton(
         style: TextButton.styleFrom(
-            backgroundColor: Colors.blue,
+            backgroundColor: cardColor[i],
             foregroundColor: Colors.white,
             textStyle: TextStyle(fontSize: 40)),
         onPressed: () => pressing(i),
